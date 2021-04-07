8a9a081ecf7c87b285f632f3e8f068c3483b8b2a
rohan-04 <mathurrohan04@gmail.com>
2021-04-08 00:00:27 +0530

    use pug template engine

--- app.js
+++ app.js
@@ -3,23 +3,28 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
 
+// app.set(---) sets global value which can be use in our whole app
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
 // For parsing incoming request
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// For serving static files i.e CSS files or images, etc
+// For serving static files i.e CSS files or images or javascript, etc
+//  __dirname gives path till current file in pc
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Defining Routes
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use('/', shopRoutes);
 
 app.use('/', (req, res, next) => {
-	res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+	res.status(404).render('404');
 });
 
 // const server = http.createServer(app);
--- routes/admin.js
+++ routes/admin.js
@@ -3,20 +3,24 @@ const path = require('path');
 const express = require('express');
 
 const rootDir = require('../util/path');
+const e = require('express');
 
 const router = express.Router();
+
+const products = [];
 
 // We can have same path if request method differs
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-	res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+	res.render('add-product', { pageTitle: 'Add Product' });
 });
 
 // /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
-	console.log(req.body);
+	products.push({ title: req.body.title });
 	res.redirect('/');
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
