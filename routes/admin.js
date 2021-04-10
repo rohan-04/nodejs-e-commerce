1ad248583b3607a82e1a195ad8a01c7f92376c1f
rohan-04 <mathurrohan04@gmail.com>
2021-04-10 21:59:17 +0530

    added more controllers for shop

--- routes/admin.js
+++ routes/admin.js
@@ -1,18 +1,18 @@
 const path = require('path');
 const express = require('express');
-const productsController = require('../controllers/shop');
+const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
 // We can have same path if request method differs
 
 // /admin/add-product => GET
-router.get('/add-product', productsController.getAddProduct);
+router.get('/add-product', adminController.getAddProduct);
 
 // /admin/products => GET
 router.get('/products');
 
 // /admin/add-product => POST
-router.post('/add-product', productsController.postAddProduct);
+router.post('/add-product', adminController.postAddProduct);
 
 module.exports = router;
