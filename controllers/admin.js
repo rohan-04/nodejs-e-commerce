51e277dfe49b0080f6778f56e71c6a2d9035ea28
rohan-04 <mathurrohan04@gmail.com>
2021-04-12 23:41:10 +0530

    pre-populated the edit form

--- controllers/admin.js
+++ controllers/admin.js
@@ -4,6 +4,7 @@ exports.getAddProduct = (req, res, next) => {
 	res.render('admin/edit-product', {
 		pageTitle: 'Add Product',
 		path: '/admin/add-product',
+		editing: false,
 	});
 };
 
@@ -20,14 +21,22 @@ exports.postAddProduct = (req, res, next) => {
 // Using query params
 exports.getEditProduct = (req, res, next) => {
 	const editMode = req.query.edit;
-	// console.log(req.query);
+	// console.log(req.query.edit);
 	if (!editMode) {
 		return res.redirect('/');
 	}
-	res.render('admin/edit-product', {
-		pageTitle: 'Add Product',
-		path: '/admin/add-product',
-		editing: editMode,
+	const prodId = req.params.productId;
+
+	Product.findById(prodId, (product) => {
+		if (!product) {
+			return res.redirect('/');
+		}
+		res.render('admin/edit-product', {
+			pageTitle: 'Edit Product',
+			path: '/admin/edit-product',
+			editing: editMode,
+			product: product,
+		});
 	});
 };
 
