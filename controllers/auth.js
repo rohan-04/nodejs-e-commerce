dc91eff773e8dd3f441cc5dede96aa9d9dd281a3
rohan-04 <mathurrohan04@gmail.com>
2021-04-26 21:17:23 +0530

    added login functionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -22,15 +22,30 @@ exports.getSignup = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-	User.findById('6084614c503bcf07e0435362')
+	const email = req.body.email;
+	const password = req.body.password;
+	User.findOne({ email: email })
 		.then((user) => {
-			// Creating a session
-			req.session.isLoggedIn = true;
-			req.session.user = user;
-			req.session.save((err) => {
-				console.log(err);
-				res.redirect('/');
-			});
+			if (!user) {
+				return res.redirect('/login');
+			}
+			bcrypt
+				.compare(password, user.password)
+				.then((doMatch) => {
+					if (doMatch) {
+						// Creating a session
+						req.session.isLoggedIn = true;
+						req.session.user = user;
+						return req.session.save((err) => {
+							console.log(err);
+							res.redirect('/');
+						});
+					}
+					res.redirect('/login');
+				})
+				.catch((err) => {
+					console.log(err);
+				});
 		})
 		.catch((err) => console.log(err));
 };
