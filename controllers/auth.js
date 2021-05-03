39dc2f80c9be13ba35a8781d0994537fabf1df67
rohan-04 <mathurrohan04@gmail.com>
2021-05-03 22:21:28 +0530

    added more validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -91,12 +91,11 @@ exports.postSignup = (req, res, next) => {
 	const errors = validationResult(req);
 
 	if (!errors.isEmpty()) {
-		console.log(errors.array());
 		// Status code 422 in validation failed error
 		return res.status(422).render('auth/signup', {
 			path: '/signup',
 			pageTitle: 'Signup',
-			errorMessage: errors.array(),
+			errorMessage: errors.array()[0].msg,
 		});
 	}
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { check } = require('express-validator');
+const { check, body } = require('express-validator');
 
 const authController = require('../controllers/auth');
 
@@ -11,7 +11,25 @@ router.get('/signup', authController.getSignup);
 
 router.post('/login', authController.postLogin);
 
-router.post('/signup', check('email').isEmail(), authController.postSignup); // Check() for validation
+router.post(
+	'/signup',
+	[
+		check('email').isEmail().withMessage('Please enter a valid email.'),
+		body(
+			'password',
+			'Please enter a password with only numbers ,text and atleast 5 characters.'
+		)
+			.isLength({ min: 5 })
+			.isAlphanumeric(),
+		body('confirmPassword').custom((value, { req }) => {
+			if (value !== req.body.password) {
+				throw new Error('Password does not match !');
+			}
+			return true;
+		}),
+	],
+	authController.postSignup
+);
 
 router.post('/logout', authController.postLogout);
 
